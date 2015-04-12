528fd9226e7fd29abb17254ac90b2587087bace1
gifnksm <makoto.nksm+github@gmail.com>
2015-04-12 15:53:02 +0900

    gulpfile.js: refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,24 @@ var through = require("through2");
 var uglify = require("gulp-uglify");
 var karma = require("karma").server;
 var watchify = require("watchify");
+var rename = require("gulp-rename");
+var buffer = require("gulp-buffer");
+
+function pathConverter(base) {
+  return function(path) {
+    if (path === undefined) {
+      return base;
+    }
+    return base + path;
+  };
+}
+
+var Path = {
+  input: pathConverter("./"),
+  compiled: pathConverter("./target/compiled/"),
+  bundle: pathConverter("./target/bundle/"),
+  dist: pathConverter("./target/dist/")
+};
 
 var tsProject = typescript.createProject({
   typescript: require("typescript"),
@@ -26,23 +44,25 @@ function typescript_build(src, option) {
       .pipe(sourcemaps.init())
       .pipe(typescript(tsProject))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest("./target/compiled/"));
+      .pipe(gulp.dest(Path.compiled()));
   };
 }
 
 gulp.task("build:typescript",
-          typescript_build(["./src/**/*.ts", "./typings/**/*.d.ts"],
-                           {base: './'}));
-gulp.task("test:typescript",
-          typescript_build(["./test/**/*.ts", "./src/**/*.ts", "./typings/**/*.d.ts"],
-                           {base: './'}));
+          typescript_build(Path.input("src/**/*.ts"), {base: Path.input()}));
+gulp.task("test:typescript", ["build:typescript"],
+          typescript_build(Path.input("test/**/*.ts"), {base: Path.input()}));
 
 
 gulp.task("build:bundle", ["build:typescript"], function() {
-  return browserify("./target/compiled/src/index.js")
+  return browserify(Path.compiled("src/index.js"))
     .bundle()
     .pipe(source("index.js"))
-    .pipe(gulp.dest("./target/bundle/src/"));
+    .pipe(gulp.dest(Path.bundle("src")))
+    .pipe(rename({extname: ".min.js"}))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(Path.bundle("src")));
 });
 
 
@@ -52,38 +72,43 @@ function test_bundle(isWatch) {
       return bundler
         .bundle()
         .pipe(source(src.replace(/.*\/target\/compiled\//, '')))
-        .pipe(gulp.dest('./target/bundle/'));
+        .pipe(gulp.dest(Path.bundle()));
     };
   }
 
-  function browserified() {
-    return through.obj(function(chunk, enc, callback) {
+  function transform(fun) {
+    return through.obj(function(chunk, end, cb) {
       if (chunk.isBuffer()) {
-        var option = {debug: true};
-        if (isWatch) {
-          option.cache = {};
-          option.packageCache = {};
-        }
-        var bundler = browserify(chunk.path, option);
-        bundler.transform('espowerify');
-
-        if (isWatch) {
-          bundler = watchify(bundler);
-          bundler.on('update', bundle(chunk.path, bundler));
-        }
-
-        chunk.contents = bundler.bundle();
+        chunk.contents = fun(chunk.path);
         this.push(chunk);
       }
-      callback();
+      cb();
+    });
+  }
+
+  function browserified() {
+    return transform(function(path) {
+      var option = {debug: true};
+      if (isWatch) {
+        option.cache = {};
+        option.packageCache = {};
+      }
+
+      var bundler = browserify(path, option);
+      bundler.transform('espowerify');
+
+      if (isWatch) {
+        bundler = watchify(bundler);
+        bundler.on('update', bundle(path, bundler));
+      }
+      return bundler.bundle();
     });
   }
 
   return function() {
-    return gulp.src("./target/compiled/test/**/*.js",
-                    {base: './target/compiled/'})
+    return gulp.src(Path.compiled("test/**/*.js"), {base: Path.compiled()})
       .pipe(browserified())
-      .pipe(gulp.dest("./target/bundle/"));
+      .pipe(gulp.dest(Path.bundle()));
   };
 }
 
@@ -91,8 +116,20 @@ gulp.task("test:bundle", ["test:typescript"], test_bundle(false));
 gulp.task("watch:bundle", ["test:typescript"], test_bundle(true));
 
 
-function userscript_meta(baseName) {
-  return function () {
+var userscriptBaseName = {
+  normal: "nicovideothumbinfopopup",
+  min: "nicovideothumbinfopopup.min"
+};
+var bundleSrcName = {
+  normal: "index.js",
+  min: "index.min.js"
+};
+
+["normal", "min"].forEach(function(type) {
+  var baseName = userscriptBaseName[type];
+  var src = bundleSrcName[type];
+
+  gulp.task("build:meta:" + type, function () {
     var baseUrl = "https://raw.githubusercontent.com/gifnksm/nicovideo-thumbinfo-popup/master/";
     return gulp.src("./etc/userscript/header.txt")
       .pipe(template({
@@ -100,39 +137,16 @@ function userscript_meta(baseName) {
         updateUrl: baseUrl + baseName + ".meta.js",
         downloadUrl: baseUrl + baseName + ".user.js"
       }))
-      .pipe(concat(baseName + ".meta.js"))
-      .pipe(gulp.dest("./target/"));
-  };
-}
+      .pipe(rename(baseName + ".meta.js"))
+      .pipe(gulp.dest(Path.dist()));
+  });
 
-var userscript_normal_baseName = "nicovideothumbinfopopup";
-var userscript_min_baseName = "nicovideothumbinfopopup.min";
-
-gulp.task("build:meta:normal", userscript_meta(userscript_normal_baseName));
-gulp.task("build:meta:min", userscript_meta(userscript_min_baseName));
-
-function userscript_body(baseName, body) {
-  return function() {
-    return merge(
-      gulp.src("./target/" + baseName + ".meta.js"),
-      body()
-    )
+  gulp.task("build:" + type, ["build:meta:" + type, "build:bundle"], function() {
+    gulp.src([Path.dist(baseName + ".meta.js"), Path.bundle("src/" + src)])
       .pipe(concat(baseName + ".user.js"))
-      .pipe(gulp.dest("./target/"));
-  };
-}
-
-gulp.task("build:normal", ["build:meta:normal", "build:bundle"],
-          userscript_body(userscript_normal_baseName, function() {
-            return gulp.src("./target/bundle/src/index.js");
-          }));
-gulp.task("build:min", ["build:meta:min", "build:bundle"],
-          userscript_body(userscript_min_baseName, function() {
-            return gulp.src("./target/bundle/src/index.js")
-              .pipe(uglify());
-          }));
-
-
+      .pipe(gulp.dest(Path.dist()));
+  });
+});
 
 gulp.task("build", ["build:normal", "build:min"]);
 
@@ -153,7 +167,9 @@ gulp.task("watch", ["watch:bundle"], function() {
     autoWatch: true
   });
 
-  return gulp.watch(["test/**/*.ts", "src/**/*.ts", "typings/**/*.d.ts"],
+  return gulp.watch([Path.input("test/**/*.ts"),
+                     Path.input("src/**/*.ts"),
+                     "typings/**/*.d.ts"],
                     ["test:typescript"]);
 });
 

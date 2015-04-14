947fb399f10f5a87e042dccabb06026ae6f8554f
gifnksm <makoto.nksm+github@gmail.com>
2015-04-14 23:38:58 +0900

    NicoiThumbinfoData: Support merging multi-source data

--- src/model/NicoThumbinfoData.ts
+++ src/model/NicoThumbinfoData.ts
@@ -3,21 +3,58 @@
 import {Option, Some, None} from "option-t";
 import Key from "NicoThumbinfoKey";
 
-class Tag {
-    category: boolean;
-    lock: boolean;
+class Thread {
+    id: string;
+    numRes: string;
 }
 
-class ThumbData {
-    private _key: Key;
+class Tag {
+    isCategory: boolean;
+    isLocked: boolean;
+}
+
+interface Uploader {
+    id: string;
+    name: string;
+    iconUrl: string;
+    url: string;
+}
+
+class User implements Uploader {
+    id: string;
+    name: string;
+    iconUrl: string;
+    get url(): string {
+        return "http://www.nicovideo.jp/user/" + this.id
+    }
+}
+
+class Channel implements Uploader {
+    id: string;
+    name: string;
+    iconUrl: string;
+    get url(): string {
+        return "http://ch.nicovideo.jp/channel/ch" + this.id;
+    }
+}
+
+enum Source {
+    WatchPage = 0,
+    V3VideoArray = 1,
+    GetThumbinfo = 2
+}
+
+export class RawThumbData {
+    key: Key;
+    source: Source;
 
     thumbType: string;
-
     videoId: string;
-    title: string;
+    threadId: string;
+
     description: string;
     thumbnailUrl: string;
-    firstRetrieve: string;
+    postedAt: string;
     length: string;
 
     viewCounter: string;
@@ -25,26 +62,74 @@ class ThumbData {
     mylistCounter: string;
     lastResBody: string;
 
-    embeddable: boolean;
-    livePlay: boolean;
-
     tags: {[index: string]: Tag};
+    uploader: Uploader;
 
-    userId: string;
-    userNickname: string;
-    userIconUrl: string;
+    merge(rawData: RawThumbData) {
+        // key と source はマージしない
+        if (this.thumbType === undefined) { this.thumbType = rawData.thumbType; }
+        if (this.videoId === undefined) { this.videoId = rawData.videoId; }
+        if (this.threadId === undefined) { this.threadId = rawData.threadId; }
 
-    chId: string;
-    chName: string;
-    chIconUrl: string;
+        if (this.description === undefined) { this.description = rawData.description; }
+        if (this.thumbnailUrl === undefined) { this.thumbnailUrl = rawData.thumbnailUrl; }
+        if (this.postedAt === undefined) { this.postedAt = rawData.postedAt; }
+        if (this.length === undefined) { this.length = rawData.length; }
+
+        if (this.viewCounter === undefined) { this.viewCounter = rawData.viewCounter; }
+        if (this.commentCounter === undefined) { this.commentCounter = rawData.commentCounter; }
+        if (this.mylistCounter === undefined) { this.mylistCounter = rawData.mylistCounter; }
+        if (this.lastResBody === undefined) { this.lastResBody = rawData.lastResBody; }
+        if (this.tags === undefined) { this.tags = rawData.tags; }
+        if (this.uploader === undefined) { this.uploader = rawData.uploader; }
+    }
+}
+
+class ThumbData {
+    private _key: Key;
+    private _primaryData: Option<RawThumbData>;
+
+    private _rawData: RawThumbData[] = [];
 
     constructor(key: Key) {
         this._key = key;
     }
 
-    get key(): Key {
-        return this._key;
+    private _updatePrimaryData() {
+        if (this._primaryData.isSome) {
+            return;
+        }
+
+        let data = new RawThumbData();
+        for (let rawData of this._rawData) {
+            data.merge(rawData);
+        }
+        this._primaryData = new Some(data);
     }
+
+    private _getPrimaryData() {
+        if (!this._primaryData.isSome) {
+            this._updatePrimaryData();
+        }
+        return this._primaryData.unwrap();
+    }
+
+    pushRawData(raw: RawThumbData) {
+        this._rawData.push(raw);
+        this._rawData.sort((a, b) => a.source - b.source);
+        this._primaryData = new None<RawThumbData>();
+    }
+
+    get key(): Key { return this._key; }
+    get thumbType() { return this._getPrimaryData().thumbType; }
+    get videoId() { return this._getPrimaryData().videoId; }
+    get description() { return this._getPrimaryData().description; }
+    get thumbnailUrl() { return this._getPrimaryData().thumbnailUrl; }
+    get postedAt() { return this._getPrimaryData().postedAt; }
+    get length() { return this._getPrimaryData().length; }
+    get viewCounter() { return this._getPrimaryData().viewCounter; }
+    get commentCounter() { return this._getPrimaryData().commentCounter; }
+    get mylistCounter() { return this._getPrimaryData().mylistCounter; }
 
     get watchUrl(): string {
         return `http://www.nicovideo.jp/watch/${this._key.id}`;
